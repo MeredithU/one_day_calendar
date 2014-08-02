a45ec8ed779c8dd157fe156291ff094c13d978c1
MeredithU <me@meredithunderell.com>
2014-08-01 20:48:46 -0700

    Created two JS files. Events are mapped out on the calendar.

--- js/calendar.js
+++ js/calendar.js
@@ -1,5 +1,22 @@
 function layOutDay(events) {
 	events.forEach(function(entry) {
-		console.log(entry);
+		var calendarGrid = document.getElementById("grid");
+		var currentEvent = entry;
+		var startTime = entry.start;
+		var endTime = entry.end;
+
+		currentEvent.height = endTime - startTime;
+
+		console.log("Box height:" + currentEvent.height);
+
+		var timeBlockDiv = document.createElement("div");
+		timeBlockDiv.className = "timeBlock";
+		timeBlockDiv.style.height = (endTime - startTime) + "px";
+		timeBlockDiv.style.width = "600px";
+		timeBlockDiv.style.position = "absolute";
+		timeBlockDiv.style.top = (startTime + 30) + "px";
+		timeBlockDiv.style.left = "120" + "px";
+
+		calendarGrid.appendChild(timeBlockDiv);
 	});
 };
--- js/main.js
+++ js/main.js
@@ -19,23 +19,23 @@ function calendarLayoutInit() {
 		if (i < 13) {
 			var topOfHourList = document.createElement("li");
 			topOfHourList.className = "topOfHour";
-			var pElement = document.createElement("p");
+			var timeSpan = document.createElement("span");
 			var hourText = document.createTextNode(i + ":00");
 			var spanElement = document.createElement("span");
 			spanElement.className = "halfHour";
 			var amOrPm = document.createTextNode("AM");
 			spanElement.appendChild(amOrPm);
-			pElement.appendChild(hourText);
-			pElement.appendChild(spanElement);
-			topOfHourList.appendChild(pElement);
+			timeSpan.appendChild(hourText);
+			timeSpan.appendChild(spanElement);
+			topOfHourList.appendChild(timeSpan);
 			unorderedList.appendChild(topOfHourList);
 
 			var halfHourList = document.createElement("li");
 			halfHourList.className = "halfHour";
-			var pElement = document.createElement("p");
+			var timeSpan = document.createElement("span");
 			var halfHourText = document.createTextNode(i + ":30");
-			pElement.appendChild(halfHourText);
-			halfHourList.appendChild(pElement);
+			timeSpan.appendChild(halfHourText);
+			halfHourList.appendChild(timeSpan);
 			unorderedList.appendChild(halfHourList);
 		}
 		else if (i >= 13) {
@@ -44,43 +44,44 @@ function calendarLayoutInit() {
 			if (twelveHourClock == 9) {
 				var topOfHourList = document.createElement("li");
 				topOfHourList.className = "topOfHour";
-				var pElement = document.createElement("p");
+				var timeSpan = document.createElement("span");
 				var hourText = document.createTextNode(twelveHourClock + ":00");
 				var spanElement = document.createElement("span");
 				spanElement.className = "halfHour";
 				var amOrPm = document.createTextNode("PM");
 				spanElement.appendChild(amOrPm);
-				pElement.appendChild(hourText);
-				pElement.appendChild(spanElement);
-				topOfHourList.appendChild(pElement);
+				timeSpan.appendChild(hourText);
+				timeSpan.appendChild(spanElement);
+				topOfHourList.appendChild(timeSpan);
 				unorderedList.appendChild(topOfHourList);
 			}
 			else {
 				var topOfHourList = document.createElement("li");
 				topOfHourList.className = "topOfHour";
-				var pElement = document.createElement("p");
+				var timeSpan = document.createElement("span");
 				var hourText = document.createTextNode(twelveHourClock + ":00");
 				var spanElement = document.createElement("span");
 				spanElement.className = "halfHour";
 				var amOrPm = document.createTextNode("PM");
 				spanElement.appendChild(amOrPm);
-				pElement.appendChild(hourText);
-				pElement.appendChild(spanElement);
-				topOfHourList.appendChild(pElement);
+				timeSpan.appendChild(hourText);
+				timeSpan.appendChild(spanElement);
+				topOfHourList.appendChild(timeSpan);
 				unorderedList.appendChild(topOfHourList);
 
 				var halfHourList = document.createElement("li");
 				halfHourList.className = "halfHour";
-				var pElement = document.createElement("p");
+				var timeSpan = document.createElement("span");
 				var halfHourText = document.createTextNode(twelveHourClock + ":30");
-				pElement.appendChild(halfHourText);
-				halfHourList.appendChild(pElement);
+				timeSpan.appendChild(halfHourText);
+				halfHourList.appendChild(timeSpan);
 				unorderedList.appendChild(halfHourList);
 			}
 		}
 	}
 
 	var calendarGridDiv = document.createElement("div");
+	calendarGridDiv.id = "grid";
 	calendarGridDiv.className = "container";
 
 	timeOfDayDiv.appendChild(unorderedList);
