b737510ee6e347a725b4daf18d0012ed3a19adf5
MeredithU <me@meredithunderell.com>
2014-08-02 22:42:06 -0700

    Conflicting events now have correct width

--- js/calendar.js
+++ js/calendar.js
@@ -1,6 +1,8 @@
 function layOutDay (events) {
-	// Declaring an array which will hold all event conflicts
-	var conflictingEvents = [];
+
+	var maxWidth = 600;
+	var conflictingEvents = []; // Declaring an array which will hold all event conflicts
+
 
 	// Sort events by their start time
 	events.sort(function (a, b) {
@@ -13,39 +15,65 @@ function layOutDay (events) {
 		return 0;
 	});
 
-	// Cycled through each event
-	events.forEach(function(entry) {
+
+	// Cycle through each event
+	events.forEach(function(entry, index) {
 		var currentEvent = entry;
-		renderEvent(currentEvent);
+		var currentEventIndex = index;
+		renderEvent(currentEvent, currentEventIndex);
 	});
 
+
 	// This function will map the events onto the calendar
-	function renderEvent (currentEvent) {
+	function renderEvent (currentEvent, currentEventIndex) {
 		var calendarGrid = document.getElementById("grid");
 		var timeBlockDiv = document.createElement("div");
+
+		var conflictingEventsList = findEventConflicts(events, currentEvent, currentEventIndex);
+		var numberOfEventConflicts = conflictingEventsList.length;
 
 		timeBlockDiv.className = "timeBlock";
 		timeBlockDiv.style.height = (currentEvent.end - currentEvent.start) + "px";
 		timeBlockDiv.style.position = "absolute";
 		timeBlockDiv.style.top = (currentEvent.start + 30) + "px";
-
-		findEventConflicts(events, currentEvent);
-
-		timeBlockDiv.style.width = "600px"; // dynamic value
-		timeBlockDiv.style.left = "120" + "px"; // dynamic value
+		timeBlockDiv.style.width = (600/numberOfEventConflicts) + "px";
+		timeBlockDiv.style.left = "120" + "px";
 
 		calendarGrid.appendChild(timeBlockDiv);
+
+		// Now that we know which events conflict with each other, update their widths
+		if (numberOfEventConflicts > 1) {
+			updateWidth(numberOfEventConflicts, currentEventIndex);
+		}
 	};
 
 
-	function findEventConflicts (allEvents, currentEvent) {
-		// Find all events the currentEvent conflicts with
-		for (var i = 0; i < allEvents.length; i++) {
-			/*if (currentEvent.start < allEvents[i].end) {
+	// If there are event conflicts, update the widths accordingly
+	function updateWidth (numberOfEventConflicts, currentEventIndex) {
+		var parentElement = document.getElementById("grid");
+	
+			for (var i = currentEventIndex; i > currentEventIndex - numberOfEventConflicts; i--) {
+				var eventchildren = parentElement.childNodes;
+				eventWidthToChange = eventchildren[i];
+				eventWidthToChange.style.width = (maxWidth / numberOfEventConflicts) + "px";
+			}
+	};
+
+
+	// Find all events the currentEvent conflicts with 
+	function findEventConflicts (allEvents, currentEvent, currentEventIndex) {
+		for (var i = 0; i <= currentEventIndex; i++) {
+			if (currentEvent.start < allEvents[i].end) {
 				conflictingEvents.push(allEvents[i]);
-			}*/
-			console.log("i:" + i + "allEvents:" + allEvents[i].end + "currentEvent: " + currentEvent.start);
+			}		
 		}
+
+		var conflictingEventsList = conflictingEvents;
+
+		// Reset the array that holds conflicting events as we loop through each new event
+		conflictingEvents = [];
+
+		return conflictingEventsList;
 	};
 
 };
